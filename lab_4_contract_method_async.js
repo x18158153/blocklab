f83ac36f7ce3ea4c82d2868a271edab8a540e483
x18158153 <x18158153@student.ncirl.ie>
2021-02-19 21:34:32 +0000

    initial code

---
+++ lab_4_contract_method_async.js
@@ -0,0 +1,7 @@
+const Web3 = require('web3')
+
+const rpcURL = '' // Your RCP URL goes here
+
+const web3 = require("web3")
+
+const rpcURL =  "https://mainnet.infura.io/v3/de9990cb56cd42e2a479a47ec959f9e0"
