6d652dd34ea4ee9fad7cde9f9d953fab5d1abbaa
x18158153 <x18158153@student.ncirl.ie>
2021-03-30 21:20:55 +0100

    added raw tx logging

--- lab4_transaction_asyn.js
+++ lab4_transaction_asyn.js
@@ -1,7 +1,10 @@
+// create  an ethereum transaction we need to be able to build it that is why we need the libraries ethereumjs and Web3 . Dependecies:
 const Tx = require('ethereumjs-tx').Transaction
 
+//second dependencies 
 const Web3 = require ('web3')
 
+//make a web3 object
 const web3 = new Web3("https://ropsten.infura.io/v3/de9990cb56cd42e2a479a47ec959f9e0")
 
 const account1= "0xB2Ee3F22D6AeFF1a870aAda0750474C070111C13" // this is the address of account 1 this guy has all the MONEH
@@ -16,6 +19,7 @@ const privatekey2 = Buffer.from('...', 'hex')
 //using a mecanisim taking 128 randomness and making  derivation path : m’/44’/60’/0’/0/0 -> make the same public/private keypair
 //Ethereum takes the public key -> hashed/choooed -> eth address
 
+//setup code send transaction to web3
 const sendTransaction = async(raw) => {
     return await web3.eth.sendSignedTransaction(raw)
 }
@@ -34,21 +38,24 @@ const transferFunds = async( account1, account2, amount)=> {
     const txObject = {
         nonce: web3.utils.toHex(txCount),
         gasLimit: web3.utils.toHex(21000),
-        gasPrice: web3.utils.toHex(web3.utils.toWei('130', 'gwei')),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
         to: account2,
         value: web3.utils.toHex(web3.utils.toWei(amount, 'ether')),
     }
 
     const tx = new Tx(txObject, {chain:'ropsten', hardfork:'petersburg'})
+    //sign the transaction object with private key ( verify with public key):
     tx.sign(privateKey1)
+
 
     const serializedTX =tx.serialize()
     const raw = '0x' + serializedTX.toString('hex')
-    let txHash = await sendTransaction(raw)
-    console.log('original object: ' + txHash)
+    console.log("raw tx in hex:" + raw)
+    let txReceipt = await sendTransaction(raw)
+    console.log('original object: ' + txReceipt)
     //console.log("err: " + txHash.err)
-    console.log("transaction hash: " +txHash.transactionHash)
-    console.log("transaction in block " + txHash.blockNumber)
+    console.log("transaction hash: " +txReceipt.transactionHash)
+    console.log("transaction in block " + txReceipt.blockNumber)
 
 }
  
