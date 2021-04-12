160b4eae65c1bbbb929f7bb77d5120c37cc6b9a6
x18158153 <72471529+x18158153@users.noreply.github.com>
2021-04-12 22:46:28 +0100

    adding function to the webserver

--- handlers.js
+++ handlers.js
@@ -1,8 +1,21 @@
 const express = require("express")
-
+let erc20 = require('./lab_4_erc20_transfer_async.js')
 const app = express()
 
 app.use(express.json())
+
+//GET - read
+//POST - create a new vaule
+//PUT - update a value
+//DELETE - remove a value
+
+app.get('/symbol', async (req, res) => {
+    res.send(await erc20.getSymbol())
+})
+
+app.get('/supply', async (req, res) => {
+    res.send(await erc20.getTotalSupply())
+})
 
 const port = 8082
 app.listen(port, () => console.log ( `listening on port ${port}...`))
