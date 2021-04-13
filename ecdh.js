9553f8b07e6e4b0f88ffc64195839fcd210428e8
x18158153 <72471529+x18158153@users.noreply.github.com>
2021-04-13 20:54:05 +0100

    created alice's keypair

--- ecdh.js
+++ ecdh.js
@@ -7,6 +7,11 @@ var sodium = require('sodium-native')
 //Create Alice keypair
 var AlicePublicKey = sodium.sodium_malloc(sodium.crypto_box_PUBLICKEYBYTES)
 var AlicePrivateKey = sodium.sodium_malloc(sodium.crypto_box_SECRETKEYBYTES)
+
+sodium.crypto_box_keypair(AlicePublicKey,AlicePrivateKey)
+
+console.log(`Alice ${sodium.crypto_box_PUBLICKEYBYTES} -byte public key is: ${AlicePublicKey.toString('hex')} `)
+console.log(`Alice ${sodium.crypto_box_SECRETKEYBYTES} -byte private key is: ${AlicePrivateKey.toString('hex')} `)
 //Create bob Keypair
 
 //Alice to create secret using her private key and bob's public key
