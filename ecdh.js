6ca4f2e6154f922a97d2e9c3e8d934cc00209586
x18158153 <72471529+x18158153@users.noreply.github.com>
2021-04-13 20:59:28 +0100

    creating Bob'd keypair

--- ecdh.js
+++ ecdh.js
@@ -12,8 +12,15 @@ sodium.crypto_box_keypair(AlicePublicKey,AlicePrivateKey)
 
 console.log(`Alice ${sodium.crypto_box_PUBLICKEYBYTES} -byte public key is: ${AlicePublicKey.toString('hex')} `)
 console.log(`Alice ${sodium.crypto_box_SECRETKEYBYTES} -byte private key is: ${AlicePrivateKey.toString('hex')} `)
-//Create bob Keypair
 
+//Create bob Keypair
+var BobPublicKey = sodium.sodium_malloc(sodium.crypto_box_PUBLICKEYBYTES)
+var BobPrivateKey = sodium.sodium_malloc(sodium.crypto_box_SECRETKEYBYTES)
+
+sodium.crypto_box_keypair(BobPublicKey,BobPrivateKey)
+
+console.log(`Bob ${sodium.crypto_box_PUBLICKEYBYTES} -byte public key is: ${BobPublicKey.toString('hex')} `)
+console.log(`Bob ${sodium.crypto_box_SECRETKEYBYTES} -byte private key is: ${BobPrivateKey.toString('hex')} `)
 //Alice to create secret using her private key and bob's public key
 
 //Bob to create secret using his private key and alice's public key
