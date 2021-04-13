0c6798ee5f67c5ce89f0e4d756403a3266d69ae4
x18158153 <72471529+x18158153@users.noreply.github.com>
2021-04-13 21:13:56 +0100

    created ecdh sharedSecrets

--- ecdh.js
+++ ecdh.js
@@ -21,6 +21,20 @@ sodium.crypto_box_keypair(BobPublicKey,BobPrivateKey)
 
 console.log(`Bob ${sodium.crypto_box_PUBLICKEYBYTES} -byte public key is: ${BobPublicKey.toString('hex')} `)
 console.log(`Bob ${sodium.crypto_box_SECRETKEYBYTES} -byte private key is: ${BobPrivateKey.toString('hex')} `)
+
 //Alice to create secret using her private key and bob's public key
+var AliceSharedSecret = sodium.sodium_malloc(sodium.crypto_scalarmult_BYTES)
+sodium.sodium_memzero(AliceSharedSecret)
+
+sodium.crypto_scalarmult(AliceSharedSecret, AlicePrivateKey,BobPublicKey)
+
+console.log(`Alice created ${sodium.crypto_scalarmult_BYTES} -byte secret: ${AliceSharedSecret.toString('hex')}`)
+
 
 //Bob to create secret using his private key and alice's public key
+var BobSharedSecret = sodium.sodium_malloc(sodium.crypto_scalarmult_BYTES)
+sodium.sodium_memzero(BobSharedSecret)
+
+sodium.crypto_scalarmult(BobSharedSecret, BobPrivateKey,AlicePublicKey)
+
+console.log(`Bob created ${sodium.crypto_scalarmult_BYTES} -byte secret: ${BobSharedSecret.toString('hex')}`)
