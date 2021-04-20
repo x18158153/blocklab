7c70a0870a32e6caf1b354aa6a0648a47111e592
x18158153 <72471529+x18158153@users.noreply.github.com>
2021-04-20 21:27:50 +0100

    more stuff

--- distribute.js
+++ distribute.js
@@ -9,11 +9,20 @@ var distributionAddresses = fs.readFileSync('accounts.txt', 'utf8').split('\n');
 var ownerBalance = await contract.getBalance("0xF8e9AD434dFC24e46B8B97718eF6b95c0229621B","0xB2Ee3F22D6AeFF1a870aAda0750474C070111C13" )
 console.log(`owner balance is ${ownerBalance}`)
 
+//get 5% of owner's balance
 let tokenBalanceSplit = ownerBalance *0.05
 console.log(`5% of owner balance is ${tokenBalanceSplit}`)
 
+//get number of the addresses in the file
+let numberOfAddresses = distributionAddresses.length
+console.log (`number of addresses in file: ${numberOfAddresses}`)
+
+//get the amount to distribute per address in the file 
+let distributionAmount = tokenBalanceSplit / numberOfAddresses
+console.log(`amount to distribute per address: ${distributionAmount}`)
 //iterate  through the array and display contents
 for (looper = 0; looper < distributionAddresses.length; looper++) {
+    
  //   console.log(`address ${looper}: ${distributionAddresses[looper]}`)
 }
 }
